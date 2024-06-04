915df60cb7aedf1fb1f2d998b02aa7c163c3fe76
manishrout45 <158442860+manishrout45@users.noreply.github.com>
2024-06-05 03:31:47 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,26 +1,18 @@
-const jwt = require('jsonwebtoken'); // Ensure jsonwebtoken is installed
-const cookieParser = require('cookie-parser'); // Ensure cookie-parser is installed
-
-// Function to generate JWT token
-const generateToken = (user, message, statusCode, res) => {
-  // Generate the JWT token using a method from the user object
+export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   
-  // Determine the cookie name based on the user role
   const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
 
-  // Set cookie options
-  const cookieOptions = {
-    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Ensure cookies are only sent over HTTPS in production
-    sameSite: 'None' // Set to 'None' for cross-site requests, can also be 'Strict' or 'Lax' depending on your requirements
-  };
-
-  // Send the response with the token set in an HTTP-only cookie
   res
     .status(statusCode)
-    .cookie(cookieName, token, cookieOptions)
+    .cookie(cookieName, token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure:true,
+      sameSite: "None"
+    })
     .json({
       success: true,
       message,
@@ -28,5 +20,3 @@ const generateToken = (user, message, statusCode, res) => {
       token,
     });
 };
-
-module.exports = generateToken;
